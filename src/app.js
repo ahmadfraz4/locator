2f8449cc7667d2bae5cd2330d9ec78536d860c32
ahmadfraz4 <ahmadfraz2015@gmail.com>
2024-03-06 19:57:41 +0500

    adding package

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ let express = require('express')
 let app = express()
 let {Data} = require('./models/data.model')
 let path = require('path')
+
+const fetch = require('node-fetch');
 let port = 3000 || process.env.PORT;
 
 app.get('/',(req,res)=>{
